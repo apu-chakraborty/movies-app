0cd3777278ad319d2c6c1a1190e99510a94bff66
Apu Chakraborty <apu.chakraborty@outlook.com>
2022-05-10 17:02:36 +0530

    done banner slider

--- src/app/components/shared/slider/slider.component.ts
+++ src/app/components/shared/slider/slider.component.ts
@@ -1,16 +1,28 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger(("sliderFade"), [
+      state('void', style({opacity: 1})),
+      transition('void <=> *', [animate('1s')]), //we can use <=> to optimise below code
+      // transition('* => void', [animate('1s')])
+    ])
+  ]
 })
 export class SliderComponent implements OnInit {
   @Input()  items :Movie[] = [] ;
+  currentSlide: any = 0
     constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+this.currentSlide = ++this.currentSlide % this.items.length;
+    }, 5000)
   }
 
 }
--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -2,5 +2,5 @@ export interface Movie {
     userId: number,
     id: number,
     title: string,
-    body: string
+    body: string,
 }
