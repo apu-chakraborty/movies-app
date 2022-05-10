434a623fac820173b4917af9b389b9653cb86721
Apu Chakraborty <apu.chakraborty@outlook.com>
2022-05-10 22:19:48 +0530

    fix logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { slideInAnimation } from './app.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [slideInAnimation]
 })
 export class AppComponent implements OnInit{
   title = 'Flixlaar';
